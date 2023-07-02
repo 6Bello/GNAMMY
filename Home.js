5e837a322b4f4df8451175f4d77c89c0f96bd1f4
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-07-02 13:09:15 +0200

    Update Home.js
    riportato al mio refacotring

--- Home.js
+++ Home.js
@@ -4,57 +4,6 @@ import Recipes from './components/Recipes';
 
 
 export default function Home() {
-  const [items, setItems] = useState([]);
-  const [paddingTop, setpaddingTop] = useState(20);
-  
-  useEffect(() => {  
-    axios // Effettua una richiesta GET all'endpoint specificato utilizzando Axios
-      .get('http://192.168.1.61:3000/getRecipes')
-      .then(response => {
-        const data = response.data;        // Quando la risposta viene ricevuta con successo, assegna i dati alla costante 'data'
-        console.log(data);        // Stampa i dati sulla console
-        setItems(data);        // Imposta i dati nello stato del componente utilizzando 'setItems'
-      })
-      .catch(error => {
-        console.error(error);        // Se si verifica un errore durante la richiesta, visualizza un messaggio di errore sulla console
-
-      });
-  }, []);
-
-  const renderItem = ({ item, index }) => {
-    const timestamp = new Date().getTime(); // Genera un timestamp corrente
-    const imageUrlWithTimestamp = `http://192.168.1.61:3000/images/${item.id}?timestamp=${timestamp}`;
-    const paddingItem = ((items.length - index) * 250)-2000; // Imposta il padding inferiore in base all'indice dell'elemento
-    return (
-      <ImageBackground source={require('./R.jpg')}
-        style={{
-          width: '100%',
-          height: 200,
-          backgroundColor: '#000',
-          position: 'relative',
-        }}
-        imageStyle={{
-          resizeMode: "cover",
-          position: 'absolute',
-          width: '100%',
-          height: 200,
-          paddingBottom: paddingTop + paddingItem,
-          top: 0,
-          alignSelf: "flex-end",
-        }}
-        onScroll={handleScroll}
-      >
-        <Text style={{ color: 'black', textAlign: 'center' }}>{item.name}</Text>
-        <Text style={{ color: 'grey', textAlign: 'center' }}>{item.description}</Text>
-      </ImageBackground>
-    );
-  };
-
-  const handleScroll = (event) => {
-    const { contentOffset } = event.nativeEvent;
-    setpaddingTop(contentOffset.y);
-  };
-
   return (
     <View style={styles.container}>
       <Recipes />
