1e013a03a6ddc26012b4f3545c08a224daa17c42
Simone D'Antonio <126526583+simoodantonio@users.noreply.github.com>
2023-07-06 01:25:40 +0200

    icona?

--- App.js
+++ App.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import Home from './Home.js';
 import Search from './Search.js';
 import Account from './Account.js';
 import {Ionicons, MaterialCommunityIcons} from '@expo/vector-icons';
 import Home2 from './components/Home2.js';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Image } from 'react-native';
+import { Image, Button, IconButton } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+
+const HeaderRightButton = () => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate('Search');
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress} style={{ marginRight: 15 }}>
+      <Ionicons name="search" size={24} color="white" />
+    </TouchableOpacity>
+  );
+};
+
 
 function App() {
   return (
@@ -21,17 +38,19 @@ function App() {
             tabBarIcon: ({color, size}) => (
               <Ionicons name="ios-home" color={color} size={size}/>
             ),
-            headerRight: () => (
-              <TouchableOpacity onPress={() => {
-                return (
-                  <Search />
-                )
-              }}>
-              <Image style={{width: 25, height: 25, }} source={require("./assets/search.png")}/>
-              </TouchableOpacity>
-            )
+            // headerRight: () => (
+            //   <TouchableOpacity onPress={() => {
+            //     navigation.navigate('Search')
+            //   }}>
+            //   <Image style={{width: 25, height: 25, }} source={require("./assets/search.png")}/>
+            //   </TouchableOpacity>
+            //  )
+            headerRight: () => {
+              <HeaderRightButton />
+            }
           }
-      }/>
+        }>
+        </Tab.Screen>
         <Tab.Screen name="Search" component={Search} />
         <Tab.Screen name="Account" component={Account} options={{headerTitle:"", tabBarIcon:({color, size}) => (<MaterialCommunityIcons name='account' size={size} color={color} />)}}/>
       </Tab.Navigator>
