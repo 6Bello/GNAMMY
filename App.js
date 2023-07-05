d95a82e9feda0bb3c9e400464900ee0a89ede36f
Simone D'Antonio <126526583+simoodantonio@users.noreply.github.com>
2023-07-05 17:46:05 +0200

    bar

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './Home.js';
 import Search from './Search.js';
 import Account from './Account.js';
-import {Ionicons} from '@expo/vector-icons';
+import {Ionicons, MaterialCommunityIcons} from '@expo/vector-icons';
 import Home2 from './components/Home2.js';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,9 +16,24 @@ function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Home" component={Home2} options={
+          {headerTitle:"", 
+            tabBarIcon: ({color, size}) => (
+              <Ionicons name="ios-home" color={color} size={size}/>
+            ),
+            headerRight: () => (
+              <TouchableOpacity onPress={() => {
+                return (
+                  <Search />
+                )
+              }}>
+              <Image style={{width: 25, height: 25, }} source={require("./assets/search.png")}/>
+              </TouchableOpacity>
+            )
+          }
+      }/>
         <Tab.Screen name="Search" component={Search} />
-        <Tab.Screen name="Account" component={Account} options={{title:""}}/>
+        <Tab.Screen name="Account" component={Account} options={{headerTitle:"", tabBarIcon:({color, size}) => (<MaterialCommunityIcons name='account' size={size} color={color} />)}}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
--- components/Home2.js
+++ components/Home2.js
@@ -3,14 +3,18 @@ import { View, Text, StyleSheet, Image, TouchableOpacity, Input } from "react-na
 import { useNavigation } from "@react-navigation/native";
 import Animate from "react-animate";
 import { TextInput } from "react-native-gesture-handler";
+import Search from "../Search";
 
 export default function Home2 () {
     
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.search} type="text" >
-                <Text style={{textAlign: "center", marginLeft: -7, lineHeight: 47, fontSize: 18, fontWeight:"bold"}}>Cerca</Text>
-                <Image style={{width: 25, height: 25, position: "absolute", left: 205, top: 12, }} source={require("../assets/search.png")}/>
+            <TouchableOpacity style={styles.search} type="text" onPress={() => {
+                return (
+                    <Search />
+                )
+            }
+            }>
             </TouchableOpacity>
         </View>
     )
