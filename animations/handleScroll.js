1971657970dabcae90ed870d0bf94f3498f2ae98
6Bello <giualbamonte06@gmail.com>
2023-07-06 04:20:48 +0200

    refactoring

--- animations/handleScroll.js
+++ animations/handleScroll.js
@@ -1,6 +1,6 @@
-const handleScroll = (event) => {
+const handleScroll = (event, updatePaddingTop) => {
     const { contentOffset } = event.nativeEvent; // Ottiene lo spostamento dello scroll dall'evento
-    setpaddingTop(contentOffset.y); // Imposta il valore dello stato paddingTop con il valore di spostamento dello scroll
+    updatePaddingTop(contentOffset.y); // Imposta il valore dello stato paddingTop con il valore di spostamento dello scroll
   };
 
   export default handleScroll;
