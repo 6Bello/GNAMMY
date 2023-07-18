4270e2851f234157a4f53880e2582abcdc00b456
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-07-19 00:06:25 +0200

    likeButtonComponent

--- components/Recipes.js
+++ components/Recipes.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { Text, ImageBackground, StyleSheet } from 'react-native';
-import { View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 import { initItems } from './initItems';
 
 import Recipe from './Recipe';
-import { set } from 'react-native-reanimated';
 
 const Recipes = ({ items, updateItems, user, userFavouriteRecipes, updateUserFavouriteRecipes}) => {
     const addFavouriteRecipe = (idRecipe) => {
+        console.log("user.favouriteRecipes-before: ", user.favouriteRecipes);
         user.favouriteRecipes.push(idRecipe);
-        const updatedRecipes = [...userFavouriteRecipes, idRecipe];
+        const updatedRecipes = user.favouriteRecipes;
+        console.log("user.favouriteRecipes: ", user.favouriteRecipes);
+        console.log("updatedRecipes: ", updatedRecipes);
         updateUserFavouriteRecipes(updatedRecipes);
+        console.log("userFavouriteRecipes: ", userFavouriteRecipes);
       };
-    
-    const removeFavouriteRecipe = (idRecipe) => {
+      
+      const removeFavouriteRecipe = (idRecipe) => {
+      console.log("user.favouriteRecipes-before: ", user.favouriteRecipes);
       user.favouriteRecipes.splice(user.favouriteRecipes.indexOf(idRecipe), 1);
-      updateUserFavouriteRecipes(user.favouriteRecipes);
+      const updatedRecipes =  user.favouriteRecipes;
+      console.log("user.favouriteRecipes: ", user.favouriteRecipes);
+      console.log("updatedRecipes: ", updatedRecipes);
+      updateUserFavouriteRecipes(updatedRecipes);
+      console.log("userFavouriteRecipes: ", userFavouriteRecipes);
     }
     
     initItems(items, updateItems);  
