4c969aa9f8e01ad4b3ff983c3feb52be978336d3
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-07-28 01:51:43 +0200

    init_downloadIcon

--- components/Recipes.js
+++ components/Recipes.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Text, ImageBackground, ScrollView, StyleSheet, RefreshControl, VirtualizedList, ActivityIndicator } from 'react-native';
+import { Text, ImageBackground, ScrollView, View, StyleSheet, RefreshControl, VirtualizedList, ActivityIndicator } from 'react-native';
 import { initRecipes } from './initRecipes';
 
 import Recipe from './Recipe';
 
-const Recipes = ({ recipes, updateRecipes, idUser, isLoggedIn = false, userFavouriteRecipes = [0], setUserFavouriteRecipes, refreshing, onRefresh=()=>{}, onEndRefresh=()=>{} }) => {
+const Recipes = ({ recipes, updateRecipes, idUser, isLoggedIn = false, userFavouriteRecipes = [0], setUserFavouriteRecipes, refreshing, onRefresh = () => { }, onEndRefresh = () => { } }) => {
 
   // funzioni per aggiungere o rimuovere una ricetta dai preferiti
   const addFavouriteRecipe = (idRecipe) => {
@@ -33,51 +33,55 @@ const Recipes = ({ recipes, updateRecipes, idUser, isLoggedIn = false, userFavou
   }
 
   // Function to render each recipe item
-  const renderRecipeItem = ({ item, index }) => (
-    <Recipe
-      key={index} // It's recommended to use a unique identifier for the "key" prop. Here, using the index as a temporary solution.
-      idUser={idUser}
-      isLoggedIn={isLoggedIn}
-      item={item}
-      index={index}
-      updateRecipes={updateRecipes}
-      recipes={recipes}
-      userFavouriteRecipes={userFavouriteRecipes}
-      addFavouriteRecipe={addFavouriteRecipe}
-      removeFavouriteRecipe={removeFavouriteRecipe}
-    />
-  );
-  const getItemCount = () => recipes!=undefined ? recipes.length : 0;
-  
+  const renderRecipeItem = ({ item, index }) => {
+    if (index === recipes.length - 1) {
+      set
+      return <View style={{ height: 100, backgroundColor: 'white' }} ><ActivityIndicator  style={{marginBottom: 20, position: 'absolute', bottom: 10}} animating={true} size="large" /></View>;
+    }
+    return (
+      <Recipe
+        key={index} // It's recommended to use a unique identifier for the "key" prop. Here, using the index as a temporary solution.
+        idUser={idUser}
+        isLoggedIn={isLoggedIn}
+        item={item}
+        index={index}
+        updateRecipes={updateRecipes}
+        recipes={recipes}
+        userFavouriteRecipes={userFavouriteRecipes}
+        addFavouriteRecipe={addFavouriteRecipe}
+        removeFavouriteRecipe={removeFavouriteRecipe}
+      />
+    )
+  };
+  const getItemCount = () => recipes != undefined ? recipes.length : 0;
+
   return (
     <VirtualizedList
-    style={styles.container}
-    data={recipes}
-    renderItem={renderRecipeItem}
-    keyExtractor={(item, index) => index.toString()}
-    getItemCount={() => recipes.length}
-    getItem={(data, index) => data[index]}
-    onEndReached={onEndRefresh} // Call the function when reaching the end of the list
-    onEndReachedThreshold={0.1}
-    refreshControl={
-      <RefreshControl
-        refreshing={refreshing}
-        onRefresh={() => {
-          onRefresh(); // Call the onRefresh function to perform the actual refresh action
-        }}
-      />
-    }
-  />
-);
+      style={styles.container}
+      data={recipes}
+      renderItem={renderRecipeItem}
+      keyExtractor={(item, index) => index.toString()}
+      getItemCount={() => recipes.length}
+      getItem={(data, index) => data[index]}
+      onEndReached={onEndRefresh} // Call the function when reaching the end of the list
+      onEndReachedThreshold={0.1}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={() => {
+            onRefresh(); // Call the onRefresh function to perform the actual refresh action
+          }}
+        />
+      }
+    />
+  );
 };
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: '100%',
     width: '100%',
+    height: '100%',
   },
 });
 
