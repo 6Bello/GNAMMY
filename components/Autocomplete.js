ced32b7a8171639f73c251c22899137e8be5e2ca
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-09-06 00:06:26 +0200

    update Co-authored-by: Cesare Covi <BiscoSwag@users.noreply.github.com>

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, FlatList, Pressable, StyleSheet, StatusBar } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown'
 import axios from 'axios';
-import { set } from 'react-native-reanimated';
+import { Ionicons } from '@expo/vector-icons';
+
+import RNSingleSelect, {
+  ISingleSelectDataType,
+} from "@freakycoder/react-native-single-select";
 
 export default function Autocomplete({ myStyle, listStyle, defaultValue, onChangeText }) {
   const [inputText, setInputText] = useState('');
@@ -54,8 +58,10 @@ export default function Autocomplete({ myStyle, listStyle, defaultValue, onChang
     setInputText(newIngredients); // Aggiorna il testo dell'input
   };
 
+  const [squareAmountIsFocused, setSquareAmountIsFocused] = useState(false); // Stato di focus del pulsante per la quantità
+  const [squareUnitIsFocused, setSquareUnitIsFocused] = useState(false); // Stato di focus del pulsante per l'unità di misura
   return (
-    <View style={myStyle}>
+    <View style={[myStyle, { display: 'flex', flexDirection: 'row', zIndex: 1000 }]}>
       <View style={{ display: 'flex', justifyContent: 'center' }}>
         <FlatList
           style={[styles.listContainerStyle, { borderColor: borderColor, borderWidth: isFocused && filteredSuggestions.length > 0 ? 1 : 0, display: isFocused ? 'flex' : 'none' }]}
@@ -72,14 +78,20 @@ export default function Autocomplete({ myStyle, listStyle, defaultValue, onChang
             </Pressable>}
           keyExtractor={(item) => item.title}
         />
+
         <TextInput
-          style={[styles.button, { borderColor: borderColor, borderWidth: 1 }]}
+          style={[styles.button, { borderColor: borderColor, borderWidth: 1, height: 40, width: 150, padding: 10, borderTopWidth: 1, justifyContent: 'center', alignItems: 'center' }]}
           placeholder="Inizia a digitare..."
           value={inputText}
           onFocus={() => setIsFocused(true)}
           onChangeText={handleInputChange}
         />
       </View>
+      <View style={{ width: '50%', height: 23, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <SquareAmount isFocused={squareAmountIsFocused} onFocus={setSquareAmountIsFocused} />
+        <SquareUnit isFocused={squareUnitIsFocused} onFocus={setSquareUnitIsFocused} />
+      </View>
+      <Ionicons name="add-circle-outline" size={30} />
       <View style={{ width: '100%', position: 'relative', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-around', width: '100%', marginTop: 5 }}>
         {ingredients.map((ingredient, index) => {
           const squareAmountIsFocused = ingredientStates[`squareAmount${index}`] || false; // Stato di focus del pulsante per la quantità
@@ -127,31 +139,55 @@ const SquareAmount = ({ isFocused, onFocus }) => {
 }
 
 const SquareUnit = ({ isFocused, onFocus }) => {
-  const [value, setValue] = useState('p');
+  const [value, setValue] = useState('');
   console.log(value);
-  console.log(data);
-  const data = [
-    { label: 'grams', value: 'gr' },
-    { label: 'portions', value: 'ps' },
+  // const data = [
+  //   { label: 'grams', value: 'gr' },
+  //   { label: 'portions', value: 'ps' },
+  // ];
+
+  const staticData = [
+    { id: 0, value: "gr" },
+    { id: 1, value: "pz" },
   ];
 
   return (
-    <View style={styles.container}>
-              <Dropdown
-          style={[styles.dropdown, isFocused && { borderColor: 'blue' }]}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          data={data}
-          maxHeight={300}
-          labelField="label"
-          valueField="value"
-          searchPlaceholder="Search..."
-          value={value}
-          onChange={item => {
-            setValue(item.value);
-          }}
-        />
+    <View style={{ width: '20%', flex: 1}}>
+      {/* <Dropdown
+        style={[styles.dropdown, isFocused && { borderColor: 'blue' }]}
+        placeholderStyle={styles.placeholderStyle}
+        containerStyle={{width: '100%', backgroundColor: 'blue'}}
+        itemTextStyle={{ fontSize: 12}}
+        selectedTextStyle={styles.selectedTextStyle}
+        listStyle={{ padding: 0, margin: 0 }}
+        data={data} 
+        maxHeight={300} 
+        labelField="label" 
+        valueField="value" 
+        placeholder='Unit'
+        value={value} 
+        onChange={item => { 
+          setValue(item.value); 
+        }}
+      /> */}
+      <RNSingleSelect width={50} height={40} menuBarContainerWidth={60} menuBarContainerBackgroundColor={'#f8f4fc'}
+        buttonContainerStyle={{ borderRadius: 0, borderTopLeftRadius: 0, borderTopRightRadius: 0, backgroundColor: '#f8f4fc', borderWidth: 1, borderColor: 'grey'}}
+        placeholderTextStyle={{ fontSize: 12, color: 'black', padding: 0, margin: 0, width: 25, right: 10, textAlign: 'center'}}
+        menuItemTextStyle={{ fontSize: 12, padding: 0, margin: 0 }}
+        menuBarContainerHeight={100}
+        menuBarContainerStyle={{ width: 50, height: 100, backgroundColor: '#f8f4fc', borderWidth: 1, borderColor: 'grey', zIndex: 9999 }}
+        menuItem
+        arrowImageStyle={styles.iconStyle}
+        placeholder='Unit'
+        menuBarTextStyle={{ fontSize: 12, padding: 0, margin: 0 }}
+        searchEnabled={false}
+        darkMode={true}
+        data={staticData}
+        onSelect={(selectedItem) =>
+          console.log("SelectedItem: ", selectedItem)
+        }
+      />
+
     </View>
   );
 }
@@ -188,11 +224,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
   dropdown: {
-    height: 50,
+    height: 25,
     borderColor: 'gray',
     borderWidth: 0.5,
     borderRadius: 8,
-    paddingHorizontal: 8,
+    paddingHorizontal: 0,
     marginBottom: 10,
   },
   icon: {
@@ -208,14 +244,16 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   placeholderStyle: {
-    fontSize: 16,
+    fontSize: 12,
   },
   selectedTextStyle: {
-    fontSize: 16,
+    fontSize: 12,
+    padding: 0,
   },
   iconStyle: {
-    width: 20,
-    height: 20,
+    width: 8,
+    height: 8,
+    right: 5,
   },
   inputSearchStyle: {
     height: 40,
