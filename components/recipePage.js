a69d4d9bdeaf8b07a836182ed575d4ea32984498
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-09-21 17:40:05 +0200

    recipesPage

--- components/recipePage.js
+++ components/recipePage.js
@@ -22,81 +22,49 @@ export default function RecipePage() {
 
     return (
         <ScrollView style={{ backgroundColor: '#FFEFAF', height: '100%', width: '100%' }}>
-            {/* <View style={{ marginTop: 10 }}>
-                <Text style={{ fontSize: 30, textAlign: 'center', fontWeight: 'bold' }}>Nome Ricetta</Text>
-
-                <View style={{ backgroundColor: 'white', height: '100%', width: '100%' }}>
-                    <View style={{ marginTop: 10 }}>
-                        <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: 'bold' }}>By {idUser}</Text>
-                        <View style={{ alignItems: 'center', padding: 10 }}>
-                            <Image style={{ width: 70, height: 70, }} source={require("../assets/hamburger.png")}></Image>
-                        </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Tempo di preparazione</Text>
-                                <Text style={{ fontSize: 15, textAlign: 'center' }}>30 minuti</Text>
-                            </View>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Difficoltà</Text>
-                                <Text style={{ fontSize: 15 }}>Facile</Text>
-                            </View>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Persone</Text>
-                                <Text style={{ fontSize: 15 }}>4</Text>
-                            </View>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Calorie</Text>
-                                <Text style={{ fontSize: 15 }}>500</Text>
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </View> */}
-            <View style={{ flex: 1, padding: 20, alignItems: 'center' }}>
-                <View style={{ backgroundColor: '#FFC90E', borderRadius: 20, width: '95%', height: '110%', alignItems: 'center', shadowColor: 'black', shadowOpacity: 0.8, shadowRadius: 5, elevation: 5, shadowOffset: { width: 0, height: 2 } }}>
-                    <View style={{ backgroundColor: '#FF7F27', borderRadius: 10, width: '85%', height: 90, marginTop: 20, alignItems: 'center', justifyContent: 'center' }}>
-                        <Text style={{ fontSize: 25, textAlign: 'center', fontWeight: 'bold', marginBottom: 5 }}>{item.title}</Text>
-                        <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: 'bold', marginTop: 5 }}>By {item.creator_username}</Text>
-                    </View>
-                    <View style={{ alignItems: 'center', padding: 10 }}>
-                        <Image style={{ width: 70, height: 70, }} source={require("../assets/hamburger.png")}></Image>
-                    </View>
-                    <View style={{ backgroundColor: '#FFFFFF', borderRadius: 10, width: '85%', marginTop: 20, alignItems: 'center' }}>
-                        <View style={{width: '80%', marginTop: 10, justifyContent: 'flex-end', flexDirection: 'row', alignItems: 'flex-end'}}>
-                            <Text style={{textAlign: 'right'}}>Contiene Glutine:</Text>
-                            {item.gluten==0 ? (<AntDesign name="closecircleo" size={20} color="red" />) : (<AntDesign name="checkcircleo" size={20} color="green" />)}
-                        </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, padding: 20 }}>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Categoria</Text>
-                                <Text style={{ fontSize: 15, textAlign: 'center' }}>{item.category}</Text>
-                            </View>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center', }}>
-                                <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>Tempo di preparazione (d/m/y)</Text>
-                                <Text style={{ fontSize: 15 }}>{item.time}</Text>
-                            </View>
-                        </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, padding: 20 }}>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{}</Text>
-                                <Text style={{ fontSize: 15 }}>{item.portions}</Text>
-                            </View>
-                            <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Difficoltà</Text>
-                                <Text style={{ fontSize: 15 }}>{item.difficulty}/5</Text>
-                            </View>
-                        </View>
-                        <View style={{ width: '50%', alignItems: 'center', justifyContent: 'center', padding: 20 }}>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Descrizione</Text>
-                            <Text style={{ fontSize: 15 }}>{item.description}</Text>
-                        </View>
-                        <View style={{ width: '60%', alignItems: 'center', justifyContent: 'center', padding: 20 }}>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Preparazione</Text>
-                            <Text style={{ fontSize: 15 }}>{item.preparation}</Text>
-                        </View>
-                    </View>
-                </View>
+        <View style={{ alignItems: 'center', padding: 20 }}>
+          <Image style={{ width: 100, height: 100, borderRadius: 50 }} source={require("../assets/hamburger.png")} />
+        </View>
+        <View style={{ backgroundColor: '#FFC90E', borderRadius: 20, width: '90%', marginHorizontal: '5%', alignItems: 'center', shadowColor: 'black', shadowOpacity: 0.8, shadowRadius: 5, elevation: 5, shadowOffset: { width: 0, height: 2 } }}>
+          <View style={{ backgroundColor: '#FF7F27', borderRadius: 10, width: '85%', height: 90, marginTop: 20, justifyContent: 'center' }}>
+            <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-around'}}>
+                <View style={{width: '33%'}}/>
+                <Text style={{ fontSize: 25, textAlign: 'center', fontWeight: 'bold'}}>{item.title}</Text>
+                <Text style={{width: '33%', fontSize: 15, textAlign: 'right' }}>{item.category}</Text>
             </View>
-        </ScrollView>
+            <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: 'bold', marginTop: 5 }}>By {item.creator_username}</Text>
+          </View>
+          <View style={{ backgroundColor: '#FFFFFF', borderRadius: 10, width: '85%', marginTop: 20, alignItems: 'center' }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 20, marginVertical: 10 }}>
+              <Text>Contiene Glutine:</Text>
+              {item.gluten === 0 ? <AntDesign name="closecircleo" size={20} color="red" /> : <AntDesign name="checkcircleo" size={20} color="green" />}
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 20, marginVertical: 10 }}>
+              <View>
+                <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>Tempo di preparazione</Text>
+                <Text style={{ fontSize: 15, textAlign: 'center' }}>{item.time}</Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 20, marginVertical: 10 }}>
+              <View>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Porzioni</Text>
+                <Text style={{ fontSize: 15, textAlign: 'center' }}>{item.portions}</Text>
+              </View>
+              <View>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Difficoltà</Text>
+                <Text style={{ fontSize: 15, textAlign: 'center' }}>{item.difficulty}/5</Text>
+              </View>
+            </View>
+            <View style={{ marginHorizontal: 20, marginVertical: 10 }}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Descrizione</Text>
+              <Text style={{ fontSize: 15 }}>{item.description}uidawhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</Text>
+            </View>
+            <View style={{ marginHorizontal: 20, marginVertical: 10 }}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Preparazione</Text>
+              <Text style={{ fontSize: 15 }}>{item.preparation}</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     );
 };
