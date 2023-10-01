e58d63bbffb9068250726c85a4677c008d2ca4f8
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-10-01 14:30:17 +0200

    refactoring

--- screens/Home.js
+++ screens/Home.js
@@ -1,78 +1,101 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Image, ImageBackground, ScrollView } from 'react-native';
 import Recipes from '../components/Recipes';
 import axios from 'axios';
 import { domain } from '../dns';
+import { ActivityIndicator } from 'react-native';
 
 export default function Home({ idUser, user, isLoggedIn, userFavouriteRecipes, setUserFavouriteRecipes }) {
-  // Stati per il caricamento delle ricette e il refresh
-  const [loading, setLoading] = useState(false);
-  const [recipes, setRecipes] = useState([]);
-  const [refreshing, setRefreshing] = useState(false);
+  //refreshing
+  const [refreshing, setRefreshing] = React.useState(false);
 
-  // Funzione per caricare le ricette
-  const loadRecipes = (params = {}) => {
-    setLoading(true);
-
-    axios
-      .get(`${domain}/getRecipes`, { params })
-      .then((response) => {
-        const data = response.data;
-        const updatedData = data.map((item) => ({
-          ...item,
-          isLiked: userFavouriteRecipes.includes(item.id),
-        }));
-
-        if (params.page === 1) {
-          setRecipes(updatedData);
-        } else {
-          setRecipes((prevRecipes) => [...prevRecipes, ...updatedData]);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => {
-        setLoading(false);
-        setRefreshing(false);
-      });
-  };
-
-  // Funzione per gestire il refresh delle ricette
-  const handleRefresh = () => {
+  const onRefresh = React.useCallback(() => {
     setRefreshing(true);
-    loadRecipes({ page: 1 }); // Eseguire una nuova ricerca quando viene eseguito il refresh
-  };
-
-  // Effettua la ricerca delle ricette quando il componente si monta
-  useEffect(() => {
-    if (isLoggedIn) {
-      // Puoi eseguire azioni specifiche quando l'utente è loggato
-    }
-
-    // Carica le ricette iniziali
-    loadRecipes({ page: 1 });
   }, []);
 
+  const [endRefreshing, setEndRefreshing] = React.useState(false);
+
+  const onEndRefresh = React.useCallback(() => {
+    setEndRefreshing(true);
+  }, []);
+
+  //get recipes
+  const [recipes, setRecipes] = useState([]); // Stato per memorizzare gli elementi ricevuti dalla ricerca
+
+  useEffect(() => {
+    if(isLoggedIn){
+      console.log(user.email)
+      // sendEmail(user.email);
+    }
+    axios // Effettua una richiesta GET all'endpoint specificato utilizzando Axios
+      .get(`${domain}/getRecipes`, {
+        params: {
+          preferences: isLoggedIn ? { antipasto: user.userPreferences.antipasto, primo: user.userPreferences.primo, secondo: user.userPreferences.secondo } : null
+        }
+      })
+      .then(response => {
+        const data = response.data;        // Quando la risposta viene ricevuta con successo, assegna i dati alla costante 'data'
+        const updatedData = data.map(item => {
+          if (userFavouriteRecipes.includes(item.id)) {
+            return { ...item, isLiked: true };
+          } else {
+            return { ...item, isLiked: false };
+          }
+        });
+        setRecipes(updatedData);        // Imposta gli elementi ottenuti come valore dello stato 'recipes'
+        console.log(updatedData.length);        // Stampa i dati sulla console
+      })
+      .catch(error => {
+        console.error(error);        // Se si verifica un errore durante la richiesta, visualizza un messaggio di errore sulla console
+      });
+    setRefreshing(false);
+  }, [refreshing]);
+
+  useEffect(() => {
+    axios // Effettua una richiesta GET all'endpoint specificato utilizzando Axios
+      .get(`${domain}/getRecipes`, {
+        params: {
+          preferences: isLoggedIn ? { antipasto: user.userPreferences.antipasto, primo: user.userPreferences.primo, secondo: user.userPreferences.secondo } : null,
+          lastRecipe: Array.isArray(recipes) && recipes.length > 0 ? recipes[recipes.length - 1].id : null
+      }
+    })
+      .then(response => {
+        const data = response.data;        // Quando la risposta viene ricevuta con successo, assegna i dati alla costante 'data'
+        const updatedData = data.map(item => {
+          if (userFavouriteRecipes.includes(item.id)) {
+            return { ...item, isLiked: true };
+          } else {
+            return { ...item, isLiked: false };
+          }
+        });
+        console.log(updatedData);        // Stampa i dati sulla console
+        setRecipes(recipes.concat(updatedData));        // Imposta gli elementi ottenuti come valore dello stato 'recipes'
+      })
+      .catch(error => {
+        console.error(error);        // Se si verifica un errore durante la richiesta, visualizza un messaggio di errore sulla console
+      });
+    setEndRefreshing(false);
+   }, [endRefreshing]);
+
+  const updateRecipes = (data) => {
+    setRecipes(data); // Aggiorna lo stato degli elementi con i risultati della ricerca
+  };
   return (
     <View style={styleContainer.container}>
       <Recipes
-        style={{ marginBottom: 20 }}
+        style={{marginBottom: 20}}
         recipes={recipes}
-        updateRecipes={loadRecipes} // Usa la funzione loadRecipes per aggiornare le ricette
+        updateRecipes={updateRecipes}
         idUser={idUser}
         isLoggedIn={isLoggedIn}
         userFavouriteRecipes={userFavouriteRecipes}
+        setUserFavouriteRecipes={setUserFavouriteRecipes}
         refreshing={refreshing}
-        onRefresh={handleRefresh}
+        onRefresh={onRefresh}
+        endRefreshing={endRefreshing}
+        onEndRefresh={onEndRefresh}
       />
-      {loading ? (
-        <ActivityIndicator
-          style={{ marginBottom: 20, position: 'absolute', bottom: 10 }}
-          animating={loading}
-          size="large"
-        />
-      ) : null}
+      {endRefreshing ? <ActivityIndicator  style={{marginBottom: 20, position: 'absolute', bottom: 10}} animating={endRefreshing} size="large" /> : null}
     </View>
   );
 }
